74cc44d0ba11463767d33f676c92754c7a1688d8
PolGuixe <pol@zetoff.com>
2016-08-29 18:32:11 +0200

    init

---
+++ client/modules/core/components/main_layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {AppBar, Navigation, Link} from 'react-toolbox';
+
+const Layout = ({
+	content = () => null
+}) => (
+	<div>
+		<div>
+			<AppBar fixed>
+				<a href="/">La Jungla</a>
+				<Navigation className="appbar-right">
+					<Link href={FlowRouter.path('product.list')} label="Test Link" icon="inbox"/>
+				</Navigation>
+			</AppBar>
+			{content()}
+		</div>
+	</div>
+);
+
+export default Layout;
