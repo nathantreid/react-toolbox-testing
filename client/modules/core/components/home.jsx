da5edacdc2fbb74e56c64ba06bb809da7ef80e41
PolGuixe <pol@zetoff.com>
2016-08-30 14:02:33 +0200

    it works

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import {Grid, Row, Col} from 'react-flexbox-grid/lib/index';
 
 const Home = () => (
-  <div>
-    <h1>Mantra</h1>
-    <p>
-      Welcome to Mantra 0.2.0.
-    </p>
-    <ul>
-      <li>
-        Read <a target="_blank" href="https://kadirahq.github.io/mantra/">spec</a>
-      </li>
-      <li>
-        Learn <a target="_blank" href="https://github.com/sungwoncho/mantra-cli#commands">CLI</a>
-      </li>
-    </ul>
-  </div>
+	<Grid>
+		<Row>
+			<Col xs={12} sm={6}>
+				<h1>Mantra</h1>
+				<p>
+					Welcome to Mantra 0.2.0.
+				</p>
+				<ul>
+					<li>
+						Read
+						<a target="_blank" href="https://kadirahq.github.io/mantra/">spec</a>
+					</li>
+					<li>
+						Learn
+						<a target="_blank" href="https://github.com/sungwoncho/mantra-cli#commands">CLI</a>
+					</li>
+				</ul>
+			</Col>
+		</Row>
+	</Grid>
 );
 
 export default Home;
